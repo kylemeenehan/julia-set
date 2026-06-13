6173363dc0f39593d5235ad010b7586b92e7e7f3
Kyle Meenehan <kylemeenehan@gmail.com>
2026-06-13 21:08:35 +0200

    start to add some layout elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import { useRef, useState, useCallback } from "react";
 import "./App.css";
 import styled from "@emotion/styled";
 import { JuliaSetCanvas } from "./JuliaSetCanvas";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import { AppBar, DrawerHeader, drawerWidth, Main } from "./layout";
+import Drawer from "@mui/material/Drawer";
+
+import { useTheme } from "@mui/material/styles";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
 
 type Vec = {
   x: number;
@@ -76,8 +89,6 @@ function getComplexCoordinateFromMouse(
 }
 
 function App() {
-  const canvas = useRef<HTMLCanvasElement>(null);
-
   // State for tracking mouse position mapped to complex plane
   const [mouseC, setMouseC] = useState<Vec>({ x: -0.742, y: 0.163 });
 
@@ -130,13 +141,72 @@ function App() {
     [setMouseC, frameInterval],
   );
 
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <>
-      <JuliaSetCanvas mouseC={mouseC} onMouseMove={handleMouseMove} />
-      <CDisplay>
-        C is ({mouseC.x}, {mouseC.y})
-      </CDisplay>
-    </>
+    <Box sx={{ display: "flex", width: "100%", height: "100%" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={[
+              {
+                mr: 2,
+              },
+              open && { display: "none" },
+            ]}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Julia Set
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List></List>
+        <Divider />
+        <List></List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <JuliaSetCanvas mouseC={mouseC} onMouseMove={handleMouseMove} />
+        <CDisplay>
+          C is ({mouseC.x}, {mouseC.y})
+        </CDisplay>
+      </Main>
+    </Box>
   );
 }
 
---
+++ src/layout.tsx
@@ -0,0 +1,66 @@
+import { styled } from "@mui/material/styles";
+import type { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import MuiAppBar from "@mui/material/AppBar";
+
+export const drawerWidth = 240;
+
+export const Main = styled("main", {
+  shouldForwardProp: (prop) => prop !== "open",
+})<{
+  open?: boolean;
+}>(({ theme }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      },
+    },
+  ],
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+export const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(["margin", "width"], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+export const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
